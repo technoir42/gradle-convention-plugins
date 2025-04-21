6cad4c9b12a70d993b2bcafff5853a629ff53f13
technoir <sergey.chelombitko@gmail.com>
2025-04-21 21:44:50 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+        google()
+    }
+}
+
+rootProject.name = "convention-plugins"
