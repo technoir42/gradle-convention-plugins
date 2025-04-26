dd240545b8738500a4444afe77cb8ebae4f79416
technoir <technoir.dev@gmail.com>
2025-04-26 23:08:30 +0100

    Introduce a concept of build features, starting with serialization

---
+++ common-conventions/src/api/kotlin/io/github/technoir42/conventions/common/api/CommonExtension.kt
@@ -0,0 +1,9 @@
+package io.github.technoir42.conventions.common.api
+
+import org.gradle.api.tasks.Nested
+
+@CommonDsl
+interface CommonExtension {
+    @get:Nested
+    val buildFeatures: CommonBuildFeatures
+}
---
+++ gradle-plugin-conventions/src/api/kotlin/io/github/technoir42/conventions/gradle/plugin/api/GradlePluginBuildFeatures.kt
@@ -0,0 +1,5 @@
+package io.github.technoir42.conventions.gradle.plugin.api
+
+import io.github.technoir42.conventions.common.api.CommonBuildFeatures
+
+interface GradlePluginBuildFeatures : CommonBuildFeatures
