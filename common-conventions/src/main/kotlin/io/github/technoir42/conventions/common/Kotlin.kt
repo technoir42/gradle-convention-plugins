c1607f4adef757a18eb162c096977b8eea5e320a
technoir <technoir.dev@gmail.com>
2025-04-28 19:26:38 +0100

    Import kotlin-bom by default

--- common-conventions/src/main/kotlin/io/github/technoir42/conventions/common/Kotlin.kt
+++ common-conventions/src/main/kotlin/io/github/technoir42/conventions/common/Kotlin.kt
@@ -1,9 +1,12 @@
 package io.github.technoir42.conventions.common
 
 import org.gradle.api.Project
+import org.gradle.api.artifacts.Dependency
 import org.gradle.api.provider.Provider
 import org.gradle.jvm.toolchain.JavaLanguageVersion
+import org.gradle.kotlin.dsl.DependencyHandlerScope
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
 import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
 
@@ -23,4 +26,11 @@ fun Project.configureKotlin(kotlinVersion: KotlinVersion = KotlinVersion.DEFAULT
             pluginManager.apply("org.jetbrains.kotlin.plugin.serialization")
         }
     }
+
+    dependencies {
+        implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    }
 }
+
+private fun DependencyHandlerScope.implementation(dependencyNotation: Any): Dependency? =
+    "implementation"(dependencyNotation)
