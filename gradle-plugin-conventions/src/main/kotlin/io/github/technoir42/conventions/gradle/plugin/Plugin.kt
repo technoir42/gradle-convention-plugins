2e4b705378af6ecac6a6066c6e26d6f0525c4741
technoir <technoir.dev@gmail.com>
2025-05-31 22:34:41 +0100

    Add functionalTest suite to Gradle plugin projects

--- gradle-plugin-conventions/src/main/kotlin/io/github/technoir42/conventions/gradle/plugin/Plugin.kt
+++ gradle-plugin-conventions/src/main/kotlin/io/github/technoir42/conventions/gradle/plugin/Plugin.kt
@@ -2,11 +2,16 @@ package io.github.technoir42.conventions.gradle.plugin
 
 import org.gradle.api.Project
 import org.gradle.api.plugins.JavaPluginExtension
+import org.gradle.api.plugins.jvm.JvmTestSuite
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.project
+import org.gradle.kotlin.dsl.register
 import org.gradle.kotlin.dsl.withType
+import org.gradle.language.base.plugins.LifecycleBasePlugin
+import org.gradle.plugin.devel.GradlePluginDevelopmentExtension
 import org.gradle.plugin.devel.tasks.ValidatePlugins
+import org.gradle.testing.base.TestingExtension
 
 internal fun Project.configurePlugin() {
     configure<JavaPluginExtension> {
@@ -16,6 +21,22 @@ internal fun Project.configurePlugin() {
             withSourcesJar()
             usingSourceSet(apiSourceSet)
             capability("$group", "$name-api", "$version")
+        }
+    }
+
+    @Suppress("UnstableApiUsage")
+    configure<TestingExtension> {
+        val functionalTestSuite = suites.register<JvmTestSuite>("functionalTest") {
+            dependencies {
+                implementation.add(project())
+                implementation.add(gradleTestKit())
+            }
+        }
+        tasks.named(LifecycleBasePlugin.CHECK_TASK_NAME).configure {
+            dependsOn(functionalTestSuite)
+        }
+        configure<GradlePluginDevelopmentExtension> {
+            testSourceSet(functionalTestSuite.get().sources)
         }
     }
 
