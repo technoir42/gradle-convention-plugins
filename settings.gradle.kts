7a67c837b4fef9fb3eebedf2016337e021baaa11
technoir <technoir.dev@gmail.com>
2025-06-21 18:40:46 +0100

    Bump version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
         }
     }
     plugins {
-        val conventionPluginsVersion = providers.gradleProperty("conventionPluginsVersion").orElse("0.0.6")
+        val conventionPluginsVersion = providers.gradleProperty("conventionPluginsVersion").orElse("0.0.7")
         id("io.github.technoir42.conventions.gradle-plugin") version conventionPluginsVersion
         id("io.github.technoir42.conventions.jvm-application") version conventionPluginsVersion
         id("io.github.technoir42.conventions.jvm-library") version conventionPluginsVersion
