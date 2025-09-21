81bcdfbf5788e201bb460be0092b4f4dadeb3b34
technoir <technoir.dev@gmail.com>
2025-09-21 11:59:18 +0100

    Invoke createGraph in Metro tests

--- common-conventions/src/testFixtures/kotlin/io/technoirlab/conventions/common/fixtures/FileGeneration.kt
+++ common-conventions/src/testFixtures/kotlin/io/technoirlab/conventions/common/fixtures/FileGeneration.kt
@@ -4,25 +4,28 @@ import kotlin.io.path.createParentDirectories
 import kotlin.io.path.writeText
 
 @Suppress("SpreadOperator")
-fun GradleProject.createDependencyGraph(className: String = "com.example.AppGraph") {
+fun GradleProject.createDependencyGraph(className: String = "com.example.AppGraph") = apply {
     val packageName = className.substringBeforeLast('.')
     val classNameWithoutPackage = className.substringAfterLast('.')
     kotlinFile(className, variant = "commonMain")
         .createParentDirectories()
         .writeText(
-            //language=kotlin
+            // language=kotlin
             """
                 package $packageName
                 
                 import dev.zacsweers.metro.AppScope
                 import dev.zacsweers.metro.DependencyGraph
                 import dev.zacsweers.metro.Provides
+                import dev.zacsweers.metro.createGraph
                 
                 @DependencyGraph(AppScope::class)
                 interface $classNameWithoutPackage {
                     @Provides
                     fun provideString(): String = "hello"
                 }
+                
+                val graph = createGraph<AppGraph>()
             """.trimIndent()
         )
 }
