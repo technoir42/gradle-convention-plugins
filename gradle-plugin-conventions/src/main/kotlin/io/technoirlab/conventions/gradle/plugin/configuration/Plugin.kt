c4ef327babeab027d8d8b1e2ce0aabaea490b079
technoir <technoir.dev@gmail.com>
2025-09-23 21:02:33 +0100

    Extract 'api' variant configuration to a function

--- gradle-plugin-conventions/src/main/kotlin/io/technoirlab/conventions/gradle/plugin/configuration/Plugin.kt
+++ gradle-plugin-conventions/src/main/kotlin/io/technoirlab/conventions/gradle/plugin/configuration/Plugin.kt
@@ -5,14 +5,10 @@ import io.technoirlab.gradle.Environment
 import io.technoirlab.gradle.dependencies.api
 import io.technoirlab.gradle.setDisallowChanges
 import org.gradle.api.Project
-import org.gradle.api.artifacts.Dependency
-import org.gradle.api.attributes.java.TargetJvmEnvironment
 import org.gradle.api.plugins.JavaPluginExtension
 import org.gradle.api.plugins.jvm.JvmTestSuite
-import org.gradle.kotlin.dsl.DependencyHandlerScope
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
-import org.gradle.kotlin.dsl.named
 import org.gradle.kotlin.dsl.project
 import org.gradle.kotlin.dsl.register
 import org.gradle.kotlin.dsl.withType
@@ -21,38 +17,9 @@ import org.gradle.plugin.devel.GradlePluginDevelopmentExtension
 import org.gradle.plugin.devel.tasks.ValidatePlugins
 import org.gradle.testing.base.TestingExtension
 import org.jetbrains.dokka.gradle.DokkaExtension
-import org.jetbrains.kotlin.gradle.plugin.KotlinPlatformType
 
 internal fun Project.configurePlugin(metadata: ProjectMetadata, environment: Environment) {
-    extensions.configure(JavaPluginExtension::class) {
-        val apiSourceSet = sourceSets.create(API_VARIANT_NAME)
-
-        // TODO: Make 'api' artifact work with ABI validation
-        registerFeature(API_VARIANT_NAME) {
-            withSourcesJar()
-            usingSourceSet(apiSourceSet)
-            capability("$group", "$name-$API_VARIANT_NAME", "$version")
-        }
-
-        if (pluginManager.hasPlugin("org.jetbrains.dokka")) {
-            extensions.configure(DokkaExtension::class) {
-                dokkaSourceSets.named("main") {
-                    sourceRoots.from(apiSourceSet.allSource.srcDirs)
-                    classpath.from(apiSourceSet.compileClasspath)
-                }
-            }
-        }
-
-        // Align attributes with the main variant
-        @Suppress("NoNameShadowing")
-        configurations.named { it == apiSourceSet.apiElementsConfigurationName || it == apiSourceSet.runtimeElementsConfigurationName }
-            .configureEach {
-                attributes {
-                    attribute(TargetJvmEnvironment.TARGET_JVM_ENVIRONMENT_ATTRIBUTE, objects.named(TargetJvmEnvironment.STANDARD_JVM))
-                    attribute(KotlinPlatformType.attribute, KotlinPlatformType.jvm)
-                }
-            }
-    }
+    configureApiVariant(API_VARIANT_NAME)
 
     @Suppress("UnstableApiUsage")
     extensions.configure(TestingExtension::class) {
@@ -62,9 +29,11 @@ internal fun Project.configurePlugin(metadata: ProjectMetadata, environment: Env
                 implementation.add(gradleTestKit())
             }
         }
+
         tasks.named(LifecycleBasePlugin.CHECK_TASK_NAME).configure {
             dependsOn(functionalTestSuite)
         }
+
         extensions.configure(GradlePluginDevelopmentExtension::class) {
             website.setDisallowChanges(metadata.url)
             vcsUrl.setDisallowChanges(environment.vcsUrl)
@@ -78,21 +47,40 @@ internal fun Project.configurePlugin(metadata: ProjectMetadata, environment: Env
         }
     }
 
-    dependencies {
-        apiImplementation(gradleApi())
-        api(project(path)) {
-            capabilities {
-                requireCapability("$group:$name-$API_VARIANT_NAME")
-            }
-        }
-    }
-
     tasks.withType<ValidatePlugins>().configureEach {
         enableStricterValidation.set(true)
     }
 }
 
-private fun DependencyHandlerScope.apiImplementation(dependencyNotation: Any): Dependency? =
-    "${API_VARIANT_NAME}Implementation"(dependencyNotation)
+private fun Project.configureApiVariant(variantName: String) {
+    extensions.configure(JavaPluginExtension::class) {
+        val apiSourceSet = sourceSets.create(variantName)
+
+        // TODO: Make 'api' artifact work with ABI validation
+        registerFeature(variantName) {
+            withSourcesJar()
+            usingSourceSet(apiSourceSet)
+            capability("$group", "$name-$variantName", "$version")
+        }
+
+        if (pluginManager.hasPlugin("org.jetbrains.dokka")) {
+            extensions.configure(DokkaExtension::class) {
+                dokkaSourceSets.named("main") {
+                    sourceRoots.from(apiSourceSet.allSource.srcDirs)
+                    classpath.from(apiSourceSet.compileClasspath)
+                }
+            }
+        }
+    }
+
+    dependencies {
+        "${variantName}Implementation"(gradleApi())
+        api(project(path)) {
+            capabilities {
+                requireCapability("$group:$name-$variantName")
+            }
+        }
+    }
+}
 
 private const val API_VARIANT_NAME = "api"
