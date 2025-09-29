aa36b65102aaa99c0adda201a0b887abf2d977f2
technoir <technoir.dev@gmail.com>
2025-09-29 23:07:01 +0100

    Publish documentation

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,14 @@ plugins {
     `kotlin-dsl` apply false
     id("io.technoirlab.conventions.root")
 }
+
+dependencies {
+    dokka(project(":common-conventions"))
+    dokka(project(":gradle-plugin-conventions"))
+    dokka(project(":jvm-conventions"))
+    dokka(project(":kotlin-multiplatform-conventions"))
+    dokka(project(":root-conventions"))
+    dokka(project(":settings-conventions"))
+    dokka(project(":libraries:gradle-extensions"))
+    dokka(project(":libraries:gradle-test-kit"))
+}
