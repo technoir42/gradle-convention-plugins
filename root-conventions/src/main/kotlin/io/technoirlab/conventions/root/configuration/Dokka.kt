2f85cb9583b895ff9218d324bc896c6e93d1617b
technoir <technoir.dev@gmail.com>
2025-09-30 21:21:12 +0100

    Restore the default output directory for aggegated docs

--- root-conventions/src/main/kotlin/io/technoirlab/conventions/root/configuration/Dokka.kt
+++ root-conventions/src/main/kotlin/io/technoirlab/conventions/root/configuration/Dokka.kt
@@ -1,20 +1,12 @@
 package io.technoirlab.conventions.root.configuration
 
 import org.gradle.api.Project
-import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.withType
 import org.gradle.language.base.plugins.LifecycleBasePlugin
-import org.jetbrains.dokka.gradle.DokkaExtension
 import org.jetbrains.dokka.gradle.tasks.DokkaGeneratePublicationTask
 
 internal fun Project.configureDokka() {
     pluginManager.apply("org.jetbrains.dokka")
-
-    extensions.configure(DokkaExtension::class) {
-        dokkaPublications.named("html") {
-            outputDirectory.set(layout.projectDirectory.dir("docs"))
-        }
-    }
 
     tasks.named(LifecycleBasePlugin.BUILD_TASK_NAME) {
         dependsOn(tasks.withType(DokkaGeneratePublicationTask::class))
