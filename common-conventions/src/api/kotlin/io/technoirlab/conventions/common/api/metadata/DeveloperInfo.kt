3e1306d91c7bfc9177051118030af596fcaf07f1
technoir <technoir.dev@gmail.com>
2025-10-01 22:10:30 +0100

    Make DeveloperInfo and LicenseInfo serializable

--- common-conventions/src/api/kotlin/io/technoirlab/conventions/common/api/metadata/DeveloperInfo.kt
+++ common-conventions/src/api/kotlin/io/technoirlab/conventions/common/api/metadata/DeveloperInfo.kt
@@ -1,9 +1,19 @@
 package io.technoirlab.conventions.common.api.metadata
 
+import java.io.Serializable
+
 data class DeveloperInfo(
     val id: String?,
     val name: String,
     val email: String?,
     val organization: String?,
     val organizationUrl: String?
-)
+) : Serializable {
+
+    /**
+     * @suppress
+     */
+    companion object {
+        private const val serialVersionUID: Long = -1066512827450854604L
+    }
+}
--- common-conventions/src/api/kotlin/io/technoirlab/conventions/common/api/metadata/LicenseInfo.kt
+++ common-conventions/src/api/kotlin/io/technoirlab/conventions/common/api/metadata/LicenseInfo.kt
@@ -1,7 +1,17 @@
 package io.technoirlab.conventions.common.api.metadata
 
+import java.io.Serializable
+
 data class LicenseInfo(
     val name: String,
     val url: String,
     val distribution: String?
-)
+) : Serializable {
+
+    /**
+     * @suppress
+     */
+    companion object {
+        private const val serialVersionUID: Long = 7062271691274790485L
+    }
+}
