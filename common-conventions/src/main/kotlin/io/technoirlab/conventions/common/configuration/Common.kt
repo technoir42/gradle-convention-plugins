327547b802818a1c0fbb8c22f58e1627c6077149
technoir <technoir.dev@gmail.com>
2025-10-03 00:37:03 +0100

    Ensure that the root project has unique Maven coordinates

--- common-conventions/src/main/kotlin/io/technoirlab/conventions/common/configuration/Common.kt
+++ common-conventions/src/main/kotlin/io/technoirlab/conventions/common/configuration/Common.kt
@@ -4,6 +4,9 @@ import io.technoirlab.conventions.common.api.ProjectSettings
 import org.gradle.api.Project
 
 fun Project.configureCommon(projectSettings: ProjectSettings) {
-    group = projectSettings.groupId.get()
+    val groupId = projectSettings.groupId.get()
+    // A subproject might have the same name as the root project,
+    // but they must have a unique 'group:name:version'.
+    group = if (this == rootProject) "$groupId.root" else groupId
     version = projectSettings.version.get()
 }
