886c337aadd3351b1596d126856ce066bb049661
technoir <technoir.dev@gmail.com>
2025-10-06 22:31:55 +0100

    Publish to Maven Central

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,4 +12,13 @@ dependencies {
     dokka(project(":libraries:gradle-test-kit"))
     dokka(project(":root-conventions"))
     dokka(project(":settings-conventions"))
+
+    nmcpAggregation(project(":common-conventions"))
+    nmcpAggregation(project(":gradle-plugin-conventions"))
+    nmcpAggregation(project(":jvm-conventions"))
+    nmcpAggregation(project(":kotlin-multiplatform-conventions"))
+    nmcpAggregation(project(":libraries:gradle-extensions"))
+    nmcpAggregation(project(":libraries:gradle-test-kit"))
+    nmcpAggregation(project(":root-conventions"))
+    nmcpAggregation(project(":settings-conventions"))
 }
